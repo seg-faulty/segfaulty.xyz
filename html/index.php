6beaf04dce6a24af49eb147aec2c943f0aa82858
chas <charliehw@posteo.com>
2026-09-07 14:55:19 +0100

    first commit

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'partials/header.php';
+?>
+
+<h1>Downtime on 12th September</h1>
+<p>As the summer holidays conclude, I will be returning to University and therefore <strong>this website will be (temporarily) going down on the 12th September</strong> as I move the server hardware from my parents' house to my halls of residence.</p>
+<p>My Git server will also be affected by the downtime.</p>
+<p>Due to factors outside my control, such as road traffic and how long it will take to unpack, I cannot provide an estimate of how long this downtime will last. I hope to get this website and my Git server back up by the end of the 12th or 13th.</p>
+
+<?php
+require_once 'partials/footer.php';
+?>
+
---
+++ html/partials/header.php
@@ -0,0 +1,29 @@
+<title>Charlie's Website</title>
+<link rel=stylesheet href="/style.css">
+
+<section>
+        <div class="header">
+                <h1><a href="/">Charlie's Website</a></h1>
+        </div>
+
+	<div class="container">
+                <div class="container__sidebar">
+                        <ul class="container__sidebar__navbar">
+                                <li class="container__sidebar__navbar__header">MENU</li>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/blog">Blog</a></li>
+                                <li><a href="/projects">Projects</a></li>
+                                <li><a href="/about">About</a></li>
+                        </ul>
+                        <ul class="container__sidebar__navbar">
+                                <li class="container__sidebar__navbar__header">MY LINKS</li>
+                                <li><a href="https://git.segfaulty.xyz/chas">Gitea</a></li>
+                                <li><a href="https://bsky.app/profile/segfaulty.xyz">Bluesky</a></li>
+                                <li><a href="https://www.kamtape.com/chas">KamTape</a></li>
+                        </ul>
+
+
+
+                </div>
+		<div class="container__main">
+
