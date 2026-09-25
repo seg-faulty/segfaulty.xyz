9c2844048af9b2c418cfc0cea739ee724bd0ea64
chas <charliehw@posteo.com>
2026-09-25 17:27:34 +0100

    change to nodejs from php

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('pages/index');
+});
+
+app.get('/about', (req, res) => {
+	res.render('pages/about');
+});
+
+module.exports = app;
+
---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+
+const http = require('http');
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
